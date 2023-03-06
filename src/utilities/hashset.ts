00e5b61e2ef6d42761817a90886d0819e4fae92d
duckdoom4 <jelle.de.haan@hotmail.com>
2023-03-07 00:44:40 +0100

    Update custom containers

--- src/utilities/hashset.ts
+++ src/utilities/hashset.ts
@@ -1,5 +1,3 @@
-import { Vector } from "./vector";
-
 export interface IEquatable<T> {
     /**
      * Returns `true` if the two objects are equal, `false` otherwise.
@@ -39,5 +37,82 @@ export class ValueEqualsSet<T extends IEquatable<T>> extends Set<T> {
 
     toArray(): T[] {
         return Array.from(this);
+    }
+}
+
+/**
+ * An optimized hash set implementation which uses a hashing function and buckets to store values.
+ * @remarks This is a work in progress, and has not been tested yet.
+ */
+export class HashSet<T> implements Iterable<T> {
+    private _buckets: T[][] = [];
+    private _size = 0;
+
+    constructor(private readonly _hash: (value: T) => number) {}
+
+    get size() {
+        return this._size;
+    }
+
+    add(value: T) {
+        const hash = this._hash(value);
+        const bucket = this._buckets[hash];
+        if (bucket === undefined) {
+            this._buckets[hash] = [value];
+        } else {
+            if (!bucket.includes(value)) {
+                bucket.push(value);
+            }
+        }
+        this._size++;
+    }
+
+    addRange(values: Iterable<T>) {
+        for (const value of values) {
+            this.add(value);
+        }
+    }
+
+    has(value: T) {
+        const hash = this._hash(value);
+        const bucket = this._buckets[hash];
+        return bucket !== undefined && bucket.includes(value);
+    }
+
+    remove(value: T) {
+        const hash = this._hash(value);
+        const bucket = this._buckets[hash];
+        if (bucket !== undefined) {
+            const index = bucket.indexOf(value);
+            if (index >= 0) {
+                bucket.splice(index, 1);
+                this._size--;
+            }
+        }
+    }
+
+    [Symbol.iterator](): Iterator<T> {
+        let index = 0;
+        let bucketIndex = 0;
+        let bucket: T[] | undefined = this._buckets[0];
+        return {
+            next: () => {
+                while (bucket === undefined || index >= bucket.length) {
+                    bucket = this._buckets[++bucketIndex];
+                    index = 0;
+                }
+                return { value: bucket[index++], done: false };
+            },
+        };
+    }
+
+    toArray(): T[] {
+        const result: T[] = [];
+        for (const bucket of this._buckets) {
+            if (bucket !== undefined) {
+                result.push(...bucket);
+            }
+        }
+        return result;
     }
 }

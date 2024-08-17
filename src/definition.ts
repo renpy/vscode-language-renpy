fb15551e89ce63cd16942711283ef76895e9a27c
duckdoom4 <60387522+duckdoom4@users.noreply.github.com>
2024-08-17 13:41:49 +0200

    Test go to definition implementation

--- src/definition.ts
+++ src/definition.ts
@@ -1,17 +1,31 @@
 // Provider for Go To Definition
-import { CancellationToken, Definition, languages, Location, Position, TextDocument, Uri } from "vscode";
+import { CancellationToken, Definition, languages, Location, Position, TextDocument, Uri, window } from "vscode";
 
 import { getFileWithPath, stripWorkspaceFromFile } from "src/utilities";
 
 import { getKeywordPrefix } from "./extension";
 import { rangeAsString } from "./navigation";
 import { NavigationData } from "./navigation-data";
+import { Parser } from "./parser/parser";
 
 export function registerDefinitionProvider() {
     return languages.registerDefinitionProvider("renpy", {
         async provideDefinition(document: TextDocument, position: Position, token: CancellationToken) {
             if (token.isCancellationRequested) {
                 return;
+            }
+
+            // Test parser version if active document
+            const activeEditor = window.activeTextEditor;
+            if (activeEditor?.document === document) {
+                const program = await Parser.parseDocument(document);
+
+                const word = document.getText(document.getWordRangeAtPosition(position));
+
+                const symbol = program.globalScope.resolve(word);
+                if (symbol) {
+                    return new Location(document.uri, symbol.definitionLocation.range);
+                }
             }
 
             return Promise.resolve(getDefinition(document, position));

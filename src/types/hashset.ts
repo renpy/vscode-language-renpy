3e1bf19293c5d618586179678bfacdac28c9719e
Martin Tracey <matracey@outlook.ie>
2025-08-16 19:48:20 -0700

    chore: move IEquatable interface to types/interfaces

--- src/types/hashset.ts
+++ src/types/hashset.ts
@@ -1,15 +1,4 @@
-/**
- * Interface for types that support custom equality comparison.
- * @template T The type to compare.
- */
-export interface IEquatable<T> {
-    /**
-     * Determines whether this instance is equal to another.
-     * @param object The object to compare with.
-     * @returns `true` if the instances are equal; otherwise, `false`.
-     */
-    equals(object: T): boolean;
-}
+import { IEquatable } from "./interfaces";
 
 /**
  * A set that enforces uniqueness based on each element’s `equals` method.
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from "./hashset";
+export * from "./interfaces";
 export * from "./stack";
 export * from "./vector";

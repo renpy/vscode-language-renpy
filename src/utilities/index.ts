63f1276ef994b025e921f8ef110f48e12278ff09
Martin Tracey <matracey@outlook.ie>
2025-08-17 00:01:19 -0700

    chore: move escapeRegExpCharacters to utilities/string

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,3 +1,4 @@
+export * from "./string";
 export * from "./types";
 export * from "./utils";
 export * from "./vscode-window";
--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -1,20 +1,4 @@
 /**
- * Escapes characters in a string that have a special meaning in regular expressions.
- * This allows the string to be used as a literal part of a regular expression pattern.
- *
- * @param value The input string containing characters to be escaped.
- * @returns A new string with special regular expression characters escaped.
- * @example
- * const unsafeString = "c:\\(path)*";
- * const safeString = escapeRegExpCharacters(unsafeString);
- * // safeString is "c:\\\\\(path\)\*"
- * const regex = new RegExp(safeString);
- */
-export function escapeRegExpCharacters(value: string): string {
-    return value.replace(/[-\\{}*+?|^$.,[\]()#\s]/g, "\\$&");
-}
-
-/**
  * Wraps a promise with a timeout. If the promise does not resolve or reject
  * within the specified time, it will be rejected with a timeout error.
  *

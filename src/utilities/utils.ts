b76ed20056e89abe6aabbff138b0651ca786fd77
Martin Tracey <matracey@outlook.ie>
2025-08-16 20:29:38 -0700

    docs: added rich docs to escapeRegExpCharacters util

--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -2,6 +2,18 @@ export type EnumToString<Type> = {
     [P in keyof Type]: { name: P; value: Type[P] };
 };
 
+/**
+ * Escapes characters in a string that have a special meaning in regular expressions.
+ * This allows the string to be used as a literal part of a regular expression pattern.
+ *
+ * @param value The input string containing characters to be escaped.
+ * @returns A new string with special regular expression characters escaped.
+ * @example
+ * const unsafeString = "c:\\(path)*";
+ * const safeString = escapeRegExpCharacters(unsafeString);
+ * // safeString is "c:\\\\\(path\)\*"
+ * const regex = new RegExp(safeString);
+ */
 export function escapeRegExpCharacters(value: string): string {
     return value.replace(/[-\\{}*+?|^$.,[\]()#\s]/g, "\\$&");
 }

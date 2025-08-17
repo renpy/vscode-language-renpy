df8f7502dcc680c93e3411ccfe9c8bc30168c1fb
Martin Tracey <matracey@outlook.ie>
2025-08-16 20:25:03 -0700

    docs: added rich docs to withTimeout util

--- src/utilities/utils.ts
+++ src/utilities/utils.ts
@@ -6,6 +6,36 @@ export function escapeRegExpCharacters(value: string): string {
     return value.replace(/[-\\{}*+?|^$.,[\]()#\s]/g, "\\$&");
 }
 
+/**
+ * Wraps a promise with a timeout. If the promise does not resolve or reject
+ * within the specified time, it will be rejected with a timeout error.
+ *
+ * @template T The type of the promise's resolution value.
+ * @param promise The promise to which the timeout will be applied.
+ * @param timeout The timeout duration in milliseconds.
+ * @param onTimeout An optional callback function to execute when the timeout occurs.
+ * @param timeoutMessage An optional custom message for the rejection error on timeout.
+ * @returns A new promise that either resolves with the original promise's value
+ * or rejects if the timeout is exceeded.
+ * @throws Rejects with a timeout message if the promise does not settle within the specified time.
+ * @example
+ * async function fetchData() {
+ *   // Simulate a network request that takes 5 seconds
+ *   return new Promise(resolve => setTimeout(() => resolve('Data received'), 5000));
+ * }
+ *
+ * async function run() {
+ *   try {
+ *     // This will time out because the timeout (2000ms) is less than the task duration (5000ms)
+ *     const data = await withTimeout(fetchData(), 2000);
+ *     console.log(data);
+ *   } catch (error) {
+ *     console.error(error); // Expected output: "Promise timed out after 2000ms."
+ *   }
+ * }
+ *
+ * run();
+ */
 export async function withTimeout<T>(promise: Promise<T>, timeout: number, onTimeout?: () => void, timeoutMessage?: string): Promise<T> {
     let timerId: NodeJS.Timeout;
     const timer = new Promise<T>((_, reject) => {

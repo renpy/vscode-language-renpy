1f3b9f6731d4df3ad6ae6a5a364e0f5c969929b7
Martin Tracey <matracey@outlook.ie>
2025-08-25 02:40:18 -0700

    chore: add MochaCompositeReporter for spec, json and junit test reporting

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,4 +1,9 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { defineConfig } from "@vscode/test-cli";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
     files: "tests/**/*.test.ts",
@@ -7,6 +12,16 @@ export default defineConfig({
         ui: "bdd",
         timeout: 20000,
         require: ["ts-node/register", "tsconfig-paths/register"],
+        reporter: join(__dirname, "MochaCompositeReporter.js"),
+        reporterOptions: {
+            spec: {},
+            json: {
+                output: join(__dirname, "vscode-language-renpy-test-results.json"),
+            },
+            junit: {
+                mochaFile: join(__dirname, "vscode-language-renpy-test-results.xml"),
+            },
+        },
     },
     coverage: {
         reporter: ["text", "html", "lcov"],
